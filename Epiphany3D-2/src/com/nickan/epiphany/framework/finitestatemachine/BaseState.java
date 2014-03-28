42b823de42fb5c119313a244b5132c4e02ecd6b3
Nickan <colete_nico@yahoo.com>
2014-03-28 11:54:02 +0800

    Corrected abstract class instead of interface

--- Epiphany3D-2/src/com/nickan/epiphany/framework/finitestatemachine/BaseState.java
+++ Epiphany3D-2/src/com/nickan/epiphany/framework/finitestatemachine/BaseState.java
@@ -2,19 +2,27 @@ package com.nickan.epiphany.framework.finitestatemachine;
 
 import com.nickan.epiphany.framework.finitestatemachine.messagingsystem.Message;
 
-public abstract class BaseState<EntityType> {
-	EntityType entityType;
-	
-	private BaseState() { }
+/**
+ * The interface for the state of the entity
+ * @author Nickan
+ *
+ * @param <EntityType> - The type of entity
+ */
+public interface BaseState<EntityType> {
 	
 	public abstract void start(EntityType entity);
 	
-	public abstract void update(EntityType entity);
+	public abstract void update(EntityType entity, float delta);
 	
 	public abstract void exit(EntityType entity);
 	
-	public abstract boolean handleTelegram(Message message);
+	public abstract boolean handleMessage(EntityType entity, Message message);
 	
-	public abstract BaseState<EntityType> getInstance();
+	/**
+	 * 
+	 * 
+	 * @return - The name of the state
+	 */
+	public abstract String getStatus();
 	
 }
