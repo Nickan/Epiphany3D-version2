0f1cef43b4e3b25762b92381128872958829a894
Nickan <colete_nico@yahoo.com>
2014-03-28 11:58:15 +0800

    Just added a BoundBox for ease of collision detection

--- Epiphany3D-2/src/com/nickan/epiphany/model/Entity.java
+++ Epiphany3D-2/src/com/nickan/epiphany/model/Entity.java
@@ -2,21 +2,29 @@ package com.nickan.epiphany.model;
 
 import com.badlogic.gdx.math.Vector3;
 import com.nickan.epiphany.framework.finitestatemachine.BaseEntity;
+import com.nickan.epiphany.framework.math.BoundBox;
+import com.nickan.epiphany.framework.math.Dimension3D;
 
 public abstract class Entity extends BaseEntity {
-	Vector3 position;
-	Dimension3D dimension;
+	BoundBox boundBox;
 	
 	public Entity(Vector3 position, Dimension3D dimension) {
-		this.position = position;
-		this.dimension = dimension;
+		boundBox = new BoundBox(position, dimension);
 	}
 	
-	public final Vector3 getPosition() {
-		return position;
+	public Vector3 getPosition() {
+		return boundBox.position;
 	}
 	
-	public final Dimension3D getDimension() {
-		return dimension;
+	public Dimension3D getDimension() {
+		return boundBox.dimension;
+	}
+	
+	public BoundBox getBoundBox() {
+		return boundBox;
+	}
+	
+	public Vector3 getCenter() {
+		return boundBox.getCenter();
 	}
 }
