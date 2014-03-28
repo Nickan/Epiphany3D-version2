8afa4177bcd58399cfe9a53cedf28764082fcc0a
Nickan <colete_nico@yahoo.com>
2014-03-28 12:28:13 +0800

    Added a classes for player and zombie

---
+++ Epiphany3D-2/src/com/nickan/epiphany/model/Player.java
@@ -0,0 +1,13 @@
+package com.nickan.epiphany.model;
+
+import com.badlogic.gdx.math.Vector3;
+import com.nickan.epiphany.framework.math.Dimension3D;
+
+public class Player extends Character {
+
+	public Player(Vector3 position, Dimension3D dimension, float range, float speed) {
+		super(position, dimension, range, speed);
+		// TODO Auto-generated constructor stub
+	}
+
+}
