c12cea2d351c8dc220aadf388ac7f90f275d3314
Nickan <colete_nico@yahoo.com>
2014-03-28 12:26:22 +0800

    Added AllGameButtonsView and worldController

--- Epiphany3D-2/src/com/nickan/epiphany/gamescreen/GameScreen.java
+++ Epiphany3D-2/src/com/nickan/epiphany/gamescreen/GameScreen.java
@@ -1,34 +1,51 @@
 package com.nickan.epiphany.gamescreen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.nickan.epiphany.Epiphany;
 
 public class GameScreen implements Screen {
 	World world;
 	WorldRenderer worldRenderer;
+	WorldController worldController;
+
+	AllGameButtonsView allButtons;
+	InputMultiplexer inputPlexer;
 	
 	public GameScreen(Epiphany game) {
 		world = new World(game);
 		worldRenderer = new WorldRenderer(world);
-		Gdx.input.setInputProcessor(new WorldController(worldRenderer));
+		worldController = new WorldController(worldRenderer);
+		
+		allButtons = new AllGameButtonsView(world);
+		
+		// Set the default view of the camera
+		world.incCamRotation(15, 0, 0);
 	}
 
 	@Override
 	public void render(float delta) {
 		world.update(delta);
 		worldRenderer.render(delta);
+
+		allButtons.render(delta);
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		world.resize(width, height);
 		worldRenderer.resize(width, height);
+
+		allButtons.resize(width, height);
+		inputPlexer = new InputMultiplexer(worldController, allButtons.stage);
+		Gdx.input.setInputProcessor(inputPlexer);
 	}
 
 	@Override
 	public void show() {
 		worldRenderer.show();
+		allButtons.show();
 	}
 
 	@Override
