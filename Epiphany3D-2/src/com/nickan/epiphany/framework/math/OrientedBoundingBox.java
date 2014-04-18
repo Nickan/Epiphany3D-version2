2593f9e7c1f94b3d39dda696be763eed021edb94
Nickan <colete_nico@yahoo.com>
2014-04-18 21:44:44 +0800

    Added inverse transform and collides()

--- Epiphany3D-2/src/com/nickan/epiphany/framework/math/OrientedBoundingBox.java
+++ Epiphany3D-2/src/com/nickan/epiphany/framework/math/OrientedBoundingBox.java
@@ -13,16 +13,22 @@ public class OrientedBoundingBox {
 	private Vector3 center;
 	private Vector3 dimension;
 	private Vector3 rotation;
-	private static final Vector3 tmpVec = new Vector3();
+	
+	private static final Vector3 tmpVec1 = new Vector3();
+	private static final Vector3 tmpVec2 = new Vector3();
+	private static final Vector3 tmpVec3 = new Vector3();
+	
 	
 	private static final int CORNERS = 8;
 	private final Vector3[] corners = new Vector3[CORNERS];
 	
 	/**  For the vector of the corners, the original position  */
 	private final Vector3[] crnVec = new Vector3[CORNERS];
+	
+	private static final Vector3[] tmpCrn = new Vector3[CORNERS];
 
 	public Matrix4 transform;
-	public Matrix4 invTransform;
+	private Matrix4 invTransform = new Matrix4();
 	
 	private Matrix4 mTranslate = new Matrix4();
 	private Matrix4 mRotate = new Matrix4();
@@ -38,6 +44,7 @@ public class OrientedBoundingBox {
 		for (int index = 0; index < corners.length; ++index) {
 			crnVec[index] = new Vector3();
 			corners[index] = new Vector3();
+			tmpCrn[index] = new Vector3();
 		}
 		
 		updateDimension();
@@ -59,8 +66,8 @@ public class OrientedBoundingBox {
 	public void setToLookAt(Vector3 direction, Vector3 up) {
 		mRotate.idt();
 		// Reverse the direction and invert the rotation matrix
-		tmpVec.set(direction).scl(-1);
-		mRotate.setToLookAt(tmpVec, up);
+		tmpVec1.set(direction).scl(-1);
+		mRotate.setToLookAt(tmpVec1, up);
 		mRotate.inv();
 		updateCorners();
 	}
@@ -83,25 +90,25 @@ public class OrientedBoundingBox {
 	
 	private void updateDimension() {
 		// Imagining facing X (1, 0, 0)
-		// Front face
-		// X, -Y, -Z (Lower left)
-		crnVec[0].set(dimension.x / 2, -dimension.y / 2, -dimension.z / 2);
-		// X, -Y, Z (Lower right)
-		crnVec[1].set(dimension.x / 2, -dimension.y / 2, dimension.z / 2);
-		// X, Y, -Z (Upper left)
-		crnVec[2].set(dimension.x / 2, dimension.y / 2, -dimension.z / 2);
-		// X, Y, Z (Upper right)
-		crnVec[3].set(dimension.x / 2, dimension.y / 2, dimension.z / 2);
-
 		// Back face
+		// X, -Y, -Z (Lower left) Should be the min
+		crnVec[0].set(-dimension.x / 2, -dimension.y / 2, -dimension.z / 2);
+		// X, -Y, Z (Lower right)
+		crnVec[1].set(-dimension.x / 2, -dimension.y / 2, dimension.z / 2);
+		// X, Y, -Z (Upper left)
+		crnVec[2].set(-dimension.x / 2, dimension.y / 2, -dimension.z / 2);
+		// X, Y, Z (Upper right)
+		crnVec[3].set(-dimension.x / 2, dimension.y / 2, dimension.z / 2);
+
+		// Front face
 		// -X, -Y, -Z (Lower left)
-		crnVec[4].set(-dimension.x / 2, -dimension.y / 2, -dimension.z / 2);
+		crnVec[4].set(dimension.x / 2, -dimension.y / 2, -dimension.z / 2);
 		// -X, -Y, Z (Lower right)
-		crnVec[5].set(-dimension.x / 2, -dimension.y / 2, dimension.z / 2);
+		crnVec[5].set(dimension.x / 2, -dimension.y / 2, dimension.z / 2);
 		// -X, Y, -Z (Upper left)
-		crnVec[6].set(-dimension.x / 2, dimension.y / 2, -dimension.z / 2);
-		// -X, Y, Z (Upper right)
-		crnVec[7].set(-dimension.x / 2, dimension.y / 2, dimension.z / 2);
+		crnVec[6].set(dimension.x / 2, dimension.y / 2, -dimension.z / 2);
+		// -X, Y, Z (Upper right) Should be the max
+		crnVec[7].set(dimension.x / 2, dimension.y / 2, dimension.z / 2);
 	}
 	
 	private void updateTranslation() {
@@ -109,6 +116,29 @@ public class OrientedBoundingBox {
 		mTranslate.translate(center);
 	}
 	
+	public Matrix4 getInverseTransform() {
+		// Set the inverse scale
+		invTransform.idt();
+		m1.idt();
+//		m1.scale(1 / (dimension.x / 2), 1 / (dimension.y / 2), 1 / (dimension.z / 2) );
+//		m1.scale(1 / dimension.x, 1 / dimension.y, 1 / dimension.z);
+//		invTransform.mul(m1);
+		
+		// Set the transpose of the rotation
+		m1.idt();
+		m1.set(mRotate);
+		m1.tra();
+		invTransform.mul(m1);
+		
+		// Set the inverse translation
+		m1.idt();
+		tmpVec1.set(center);
+		tmpVec1.scl(-1);
+		m1.translate(tmpVec1);
+		
+		return invTransform.mul(m1);
+	}
+
 	private void updateRotation() {
 		// Pitch
 		m1.idt();
@@ -129,10 +159,11 @@ public class OrientedBoundingBox {
 	
 	private void updateScale() {
 		mScale.idt();
-		mScale.scale(dimension.x, dimension.y, dimension.z);
+		mScale.scale(1, 1, 1);
 	}
 	
 	private void updateCorners() {
+		// Update the transform
 		transform.idt();
 		transform.mul(mTranslate);
 		transform.mul(mRotate);
@@ -157,5 +188,111 @@ public class OrientedBoundingBox {
 	 * @return
 	 */
 	public Vector3 getRotation() { return rotation; }
+	
+	public boolean contains(Vector3 v) {
+		Matrix4 invTransform = getInverseTransform();
+		for (int index = 0; index < CORNERS; ++index) {
+			tmpCrn[index].set(corners[index]);
+			tmpCrn[index].mul(invTransform);
+		}
+		Vector3 min = tmpCrn[0];
+		Vector3 max = tmpCrn[7];
+		return contains(min, max, v);
+	}
+	
+	public static boolean contains(Vector3 min, Vector3 max, Vector3 v) {
+		return min.x <= v.x && max.x >= v.x && min.y <= v.y && max.y >= v.y && min.z <= v.z && max.z >= v.z;
+	}
+	
+	/**
+	 * Checks both box if they collide
+	 * @param box1
+	 * @param box2
+	 * @return
+	 */
+	public static boolean collides(OrientedBoundingBox box1, OrientedBoundingBox box2) {
+		Matrix4 invTransform = box1.getInverseTransform();
+		
+		tmpVec1.set(box1.getCorners()[0]);
+		tmpVec2.set(box1.getCorners()[7]);
+		Vector3 min = tmpVec1.mul(invTransform);
+		Vector3 max = tmpVec2.mul(invTransform);
+		
+		Vector3[] tmpCrn = box2.getCorners();
+		// Loop through all the transformed corners of box2 of the local space of box1
+		for (int index = 0; index < CORNERS; ++index) {
+			tmpVec3.set(tmpCrn[index]);
+			tmpVec3.mul(invTransform);
+			if (contains(min, max, tmpVec3)) {
+				return true;
+			}
+		}
+		
+		invTransform = box2.getInverseTransform();
+		// Try the local space of the box2 then
+		tmpVec1.set(box2.getCorners()[0]);
+		tmpVec2.set(box2.getCorners()[7]);
+		min = tmpVec1.mul(invTransform);
+		max = tmpVec2.mul(invTransform);
+		
+		tmpCrn = box1.getCorners();
+		// Loop through all the transformed corners of box1 of the local space of box1
+		for (int index = 0; index < CORNERS; ++index) {
+			tmpVec3.set(tmpCrn[index]);
+			tmpVec3.mul(invTransform);
+			if (contains(min, max, tmpVec3)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	/**
+	 * Returns the coordinates of the corner that collides, note that the result should
+	 * be kept as it uses static vector that will be used again. But it will return a maximum
+	 * value of the float when there is no corner collided.
+	 * @param box1
+	 * @param box2
+	 * @return
+	 */
+	public static Vector3 getCollidedCorner(OrientedBoundingBox box1, OrientedBoundingBox box2) {
+		Matrix4 invTransform = box1.getInverseTransform();
+		
+		tmpVec1.set(box1.getCorners()[0]);
+		tmpVec2.set(box1.getCorners()[7]);
+		Vector3 min = tmpVec1.mul(invTransform);
+		Vector3 max = tmpVec2.mul(invTransform);
+		
+		Vector3[] tmpCrn = box2.getCorners();
+		// Loop through all the transformed corners of box2 of the local space of box1
+		for (int index = 0; index < CORNERS; ++index) {
+			tmpVec3.set(tmpCrn[index]);
+			tmpVec3.mul(invTransform);
+			if (contains(min, max, tmpVec3)) {
+				return tmpVec3;
+			}
+		}
+		
+		invTransform = box2.getInverseTransform();
+		// Try the local space of the box2 then
+		tmpVec1.set(box2.getCorners()[0]);
+		tmpVec2.set(box2.getCorners()[7]);
+		min = tmpVec1.mul(invTransform);
+		max = tmpVec2.mul(invTransform);
+		
+		tmpCrn = box1.getCorners();
+		// Loop through all the transformed corners of box1 of the local space of box1
+		for (int index = 0; index < CORNERS; ++index) {
+			tmpVec3.set(tmpCrn[index]);
+			tmpVec3.mul(invTransform);
+			if (contains(min, max, tmpVec3)) {
+				return tmpVec3;
+			}
+		}
+		
+		return tmpVec3.set(Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
+	}
 	
 }
