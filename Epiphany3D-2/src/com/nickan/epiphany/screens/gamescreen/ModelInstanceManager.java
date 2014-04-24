caa50d6dc5a22434aeaba5fd4e7102a928e8b5af
Nickan <colete_nico@yahoo.com>
2014-04-24 11:18:18 +0800

    Manages the model instances to be drawn, if it is out of range given, it will store it but if the idle instances gets in range, it will store it back to the instances passed

---
+++ Epiphany3D-2/src/com/nickan/epiphany/screens/gamescreen/ModelInstanceManager.java
@@ -0,0 +1,56 @@
+package com.nickan.epiphany.screens.gamescreen;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Manages the array of model instances to be rendered on the screen
+ * @author Nickan
+ *
+ */
+public class ModelInstanceManager {
+	private Array<ModelInstance> idleInstances;
+	private float drawDistance;
+	private Vector3 tempPos;
+
+	public ModelInstanceManager() {
+		idleInstances = new Array<ModelInstance>();
+		tempPos = new Vector3();
+		drawDistance = 15;
+	}
+	
+	/**
+	 * Will store the instances that is out of range based on the distance from the base position and
+	 * will bring back the idle instances when they are in range.
+	 * @param instances
+	 * @param basePosition
+	 */
+	public void update(Array<ModelInstance> instances, Vector3 basePosition) {
+		for (ModelInstance instance: instances) {
+			instance.transform.getTranslation(tempPos);
+
+			// If the ModelInstance is far away from the set drawDistance
+			if (position.dst2(tempPos) > drawDistance * drawDistance) {
+				idleInstances.add(instance);
+				instances.removeValue(instance, true);
+				continue;
+			}
+		}
+
+		for (ModelInstance instance: idleInstances) {
+			instance.transform.getTranslation(tempPos);
+			// If the ModelInstance is near the drawDistance
+			if (position.dst2(tempPos) < drawDistance * drawDistance) {
+				instances.add(instance);
+				idleInstances.removeValue(instance, true);
+				continue;
+			}
+		}
+	}
+	
+	public void setDrawDistance(float drawDistance) {
+		this.drawDistance = drawDistance;
+	}
+	
+}
