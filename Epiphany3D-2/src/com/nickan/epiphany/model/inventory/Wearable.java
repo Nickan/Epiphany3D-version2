810dd800e176bef331d7063e418bd1d36a2b90be
Nickan <colete_nico@yahoo.com>
2014-05-04 21:45:38 +0800

    Renamed variables to make it clearer what are their function

--- Epiphany3D-2/src/com/nickan/epiphany/model/inventory/Wearable.java
+++ Epiphany3D-2/src/com/nickan/epiphany/model/inventory/Wearable.java
@@ -5,21 +5,21 @@ import com.nickan.epiphany.model.StatisticsHandler;
 public class Wearable extends Item {
 	public enum WearableType { HELM, ARMOR, LEFT_HAND, RIGHT_HAND, GLOVES, BOOTS };
 	private final WearableType wearableType;
-	private int addStr;
-	private int addDex;
-	private int addVit;
-	private int addAgi;
-	private int addWis;
+	private int addedStr;
+	private int addedDex;
+	private int addedVit;
+	private int addedAgi;
+	private int addedWis;
 	
-	private int addAtkDmg;
-	private int addHit;
-	private float addCrt;
-	private int addAvd;
-	private float addAtkSpd;
-	private int addDef;
+	private int addedAtkDmg;
+	private int addedHit;
+	private float addedCrit;
+	private int addedAvd;
+	private float addedAtkSpd;
+	private int addedDef;
 	
-	private int addHp;
-	private int addMp;
+	private int addedHp;
+	private int addedMp;
 	
 	public Wearable(WearableType wearableType) {
 		super(ItemClass.WEARABLE);
@@ -28,81 +28,96 @@ public class Wearable extends Item {
 	
 	/**
 	 * 
-	 * @param addStr
-	 * @param addDex
-	 * @param addVit
-	 * @param addAgi
-	 * @param addWis
+	 * @param addedStr
+	 * @param addedDex
+	 * @param addedVit
+	 * @param addedAgi
+	 * @param addedWis
 	 * @return this for chaining
 	 */
-	public Wearable setMainAttributeBonus(int addStr, int addDex, int addVit, int addAgi, int addWis) {
-		this.addStr = addStr;
-		this.addDex = addDex;
-		this.addVit = addVit;
-		this.addAgi = addAgi;
-		this.addWis = addWis;
+	public Wearable setMainAttributeBonus(int addedStr, int addedDex, int addedVit, int addedAgi, int addedWis) {
+		this.addedStr = addedStr;
+		this.addedDex = addedDex;
+		this.addedVit = addedVit;
+		this.addedAgi = addedAgi;
+		this.addedWis = addedWis;
 		return this;
 	}
 	
 	/**
 	 * 
-	 * @param addAtkDmg
-	 * @param addHit
-	 * @param addCrt
-	 * @param addDef
-	 * @param addHp
-	 * @param addAtkSpd
-	 * @param addAvd
-	 * @param addMp
+	 * @param addedAtkDmg
+	 * @param addedHit
+	 * @param addedCrit
+	 * @param addedDef
+	 * @param addedHp
+	 * @param addedAtkSpd
+	 * @param addedAvd
+	 * @param addedMp
 	 * @return this for chaining
 	 */
-	public Wearable setSubAttributeBonus(int addAtkDmg, int addHit, float addCrt, int addDef, int addHp, 
-			float addAtkSpd, int addAvd, int addMp) {
-		this.addAtkDmg = addAtkDmg;
-		this.addHit = addHit;
-		this.addCrt = addCrt;
-		this.addDef = addDef;
-		this.addHp = addHp;
-		this.addAtkSpd = addAtkSpd;
-		this.addAvd = addAvd;
-		this.addMp = addMp;
+	public Wearable setSubAttributeBonus(int addedAtkDmg, int addedHit, float addedCrit, int addedDef, int addedHp, 
+			float addedAtkSpd, int addedAvd, int addedMp) {
+		this.addedAtkDmg = addedAtkDmg;
+		this.addedHit = addedHit;
+		this.addedCrit = addedCrit;
+		this.addedDef = addedDef;
+		this.addedHp = addedHp;
+		this.addedAtkSpd = addedAtkSpd;
+		this.addedAvd = addedAvd;
+		this.addedMp = addedMp;
 		return this;
 	}
 	
 	@Override
 	public void use(StatisticsHandler statsHandler) {
-		statsHandler.addAddedStr(addStr);
-		statsHandler.addAddedDex(addDex);
-		statsHandler.addAddedVit(addVit);
-		statsHandler.addAddedAgi(addAgi);
-		statsHandler.addAddedWis(addWis);
-		statsHandler.addAddedAtkDmg(addAtkDmg);
-		statsHandler.addAddedHit(addHit);
-		statsHandler.addAddedCrt(addCrt);
-		statsHandler.addAddedDef(addDef);
-		statsHandler.addAddedHp(addHp);
-		statsHandler.addAddedAtkSpd(addAtkSpd);
-		statsHandler.addAddedAvd(addAvd);
-		statsHandler.addAddedMp(addMp);
+		statsHandler.addAddedStr(addedStr);
+		statsHandler.addAddedDex(addedDex);
+		statsHandler.addAddedVit(addedVit);
+		statsHandler.addAddedAgi(addedAgi);
+		statsHandler.addAddedWis(addedWis);
+		statsHandler.addAddedAtkDmg(addedAtkDmg);
+		statsHandler.addAddedHit(addedHit);
+		statsHandler.addAddedCrt(addedCrit);
+		statsHandler.addAddedDef(addedDef);
+		statsHandler.addAddedHp(addedHp);
+		statsHandler.addAddedAtkSpd(addedAtkSpd);
+		statsHandler.addAddedAvd(addedAvd);
+		statsHandler.addAddedHp(addedHp);
+		statsHandler.addAddedMp(addedMp);
 	}
 	
 	public void remove(StatisticsHandler statsHandler) {
-		statsHandler.addAddedStr(-addStr);
-		statsHandler.addAddedDex(-addDex);
-		statsHandler.addAddedVit(-addVit);
-		statsHandler.addAddedAgi(-addAgi);
-		statsHandler.addAddedWis(-addWis);
-		statsHandler.addAddedAtkDmg(-addAtkDmg);
-		statsHandler.addAddedHit(-addHit);
-		statsHandler.addAddedCrt(-addCrt);
-		statsHandler.addAddedDef(-addDef);
-		statsHandler.addAddedHp(-addHp);
-		statsHandler.addAddedAtkSpd(-addAtkSpd);
-		statsHandler.addAddedAvd(-addAvd);
-		statsHandler.addAddedMp(-addMp);
+		statsHandler.addAddedStr(-addedStr);
+		statsHandler.addAddedDex(-addedDex);
+		statsHandler.addAddedVit(-addedVit);
+		statsHandler.addAddedAgi(-addedAgi);
+		statsHandler.addAddedWis(-addedWis);
+		statsHandler.addAddedAtkDmg(-addedAtkDmg);
+		statsHandler.addAddedHit(-addedHit);
+		statsHandler.addAddedCrt(-addedCrit);
+		statsHandler.addAddedDef(-addedDef);
+		statsHandler.addAddedHp(-addedHp);
+		statsHandler.addAddedAtkSpd(-addedAtkSpd);
+		statsHandler.addAddedAvd(-addedAvd);
+		statsHandler.addAddedHp(-addedHp);
+		statsHandler.addAddedMp(-addedMp);
 	}
 	
-	public final WearableType getWearableType() {
-		return wearableType;
-	}
+	public final WearableType getWearableType() { return wearableType; }
+
+	public int getAddedStr() { return addedStr; }
+	public int getAddedDex() { return addedDex; }
+	public int getAddedVit() { return addedVit; }
+	public int getAddedAgi() { return addedAgi; }
+	public int getAddedWis() { return addedWis; }
+	public int getAddedAtkDmg() { return addedAtkDmg; }
+	public int getAddedHit() { return addedHit; }
+	public float getAddedCrit() { return addedCrit; }
+	public int getAddedAvd() { return addedAvd; }
+	public float getAddedAtkSpd() { return addedAtkSpd; }
+	public int getAddedDef() { return addedDef; }
+	public int getAddedHp() { return addedHp; }
+	public int getAddedMp() { return addedMp; }
+	
 }
